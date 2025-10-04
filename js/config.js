7d2698fdfc81d26d4e9ad898aadabaee75d751b1
CCCSsS66 <cccsss66619@outlook.com>
2025-10-04 21:13:51 +0800

    config.js

--- js/config.js
+++ js/config.js
@@ -69,7 +69,7 @@ const API_SITES = {
         name: '暴风资源',
     },
     tiantang: {
-        api: 'http://caiji.dyttzyapi.com/api.php/provide/vod',
+        api: 'http://caiji.dyttzyapi.com/',
         name: '电影天堂',
         detail: 'http://caiji.dyttzyapi.com'
     },
